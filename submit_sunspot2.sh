56c3d68fe3f0f86a6f5848afba9900c09574afef
Taylor Childers <taylorchilders@gmail.com>
2023-03-09 14:04:05 -0600

    updates for sunspot

--- submit_sunspot2.sh
+++ submit_sunspot2.sh
@@ -13,8 +13,13 @@ RANKS_PER_NODE=$(( GPUS_PER_NODE * RANKS_PER_GPU ))
 RANKS=$((NODES * GPUS_PER_NODE * RANKS_PER_GPU))
 echo [$SECONDS] NODES=$NODES GPUS_PER_NODE=$GPUS_PER_NODE RANKS_PER_GPU=$RANKS_PER_GPU  RANKS=$RANKS
 
+export FI_CXI_DEFAULT_CQ_SIZE=131072
+export FI_CXI_CQ_FILL_PERCENT=20
+
 export PROJ_PATH=/lus/gila/projects/atlas_aesp_CNDA/madgraph_scaling
 export MEPYTHON=/lus/gila/projects/atlas_aesp_CNDA/conda/2023-02-22/bin/python
+export MG_PROC_DIR=$PROJ_PATH/madgraph4gpu-sycl_vector/epochX/sycl/gg_ttgg.mad
+export SYCL_SETUP=setup_sycl_sunspot.sh
 
 export JOBDIR=$PROJ_PATH/jobs/$PBS_JOBID
 mkdir -p $JOBDIR
